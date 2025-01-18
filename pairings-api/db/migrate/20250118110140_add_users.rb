e28f0de0ed32517deaf9e30b7c959b61cc6e053e
marcus allen <marcusal@marcuss-MacBook-Pro-2.local>
2025-01-18 11:37:46 +0000

    add: user model, migration

---
+++ pairings-api/db/migrate/20250118110140_add_users.rb
@@ -0,0 +1,14 @@
+class AddUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :username
+      t.string :password, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
+
