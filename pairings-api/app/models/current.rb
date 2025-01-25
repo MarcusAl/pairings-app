28fe1e73ae61727f87e499d0073e2845ac4e1949
marcus allen <marcusal@marcuss-MacBook-Pro-2.local>
2025-01-25 14:29:40 +0000

    refactor: current modal to handle uuid, update bruno/swagger endpoint

--- pairings-api/app/models/current.rb
+++ pairings-api/app/models/current.rb
@@ -1,6 +1,17 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :session
-  attribute :user_agent, :ip_address
+  attribute :user_agent
+  attribute :ip_address
+  attribute :user
 
-  delegate :user, to: :session, allow_nil: true
+  def user=(user)
+    super
+    self.session = nil
+  end
+
+  def session=(session)
+    super
+    self.user = session&.user
+  end
 end
+
