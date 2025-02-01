327859b84019634fad400faacd563df53d0524b8
marcus allen <marcusal@marcuss-MacBook-Pro-2.local>
2025-02-01 14:53:42 +0000

    add: prompts yml and initiliaser config

--- pairings-api/config/initializers/anthropic.rb
+++ pairings-api/config/initializers/anthropic.rb
@@ -1,3 +1,9 @@
 Anthropic.configure do |config|
   config.access_token = ENV.fetch("ANTHROPIC_API_KEY")
+  config.headers = {
+    "anthropic-version" => "2023-06-01",
+    "content-type" => "application/json",
+  }
 end
+
+CLAUDE_PROMPT = YAML.load_file(Rails.root.join('config', 'prompts.yml'))
